57dcc40461ec154b73320d7f70961356acea0982
Navid Zare <naavidzaare@gmail.com>
2020-05-25 19:10:40 +0430

    v4-NOSQL-SQL

---
+++ CSharpOOP_Tech/NOSQL/MongoDB/MongoDB.js
@@ -0,0 +1,19 @@
+﻿show dbs // نمایش کل بانک ها
+
+db.CollectionName.insert({ JSON }) // ثبت در کالکشن
+
+show conlleciotns // نمایش تمام کالکشن ها-- db.getCollectionNames()
+
+db.ColleciotnName.find().pretty()// نمایش تمام کالکشن ها به صورت JSON
+
+db.ColleciotnName.find({ roles: 'admin' }).pretty()// شرط نمایش رول های کاربر
+
+db.CollectionName.update({ شرط }, { $set: { Name: 'ARSALAN ' } })
+
+db.CollecitonName.remove({ شرط })
+
+db.users.remove({ "age": { $gt: 25 } }); db.users.remove({ "firstName": "Dariush" }, 1); //اولین سندی ک این شرط برقرار باشد
+
+db.dropDatabase() //  برای حذف یک بانک‌اطلاعاتی، باید ابتدا باید وارد آن شده و سپس اقدام به حذف آن نماییم
+
+db.users.drop();
---
+++ CSharpOOP_Tech/SQL/FullTextSearchsql.sql
@@ -0,0 +1,41 @@
+﻿CREATE DATABASE MySearchDB
+go
+USE MySearchDB
+
+CREATE TABLE MyTBSearch ( 
+SearchId INT PRIMARY KEY IDENTITY,
+Title NVARCHAR(MAX),	
+[Description] NVARCHAR(max)
+)
+
+
+go
+INSERT INTO dbo.MyTBSearch
+(Title, Description)
+VALUES
+(   N'Best city', -- Title - nvarchar(max)
+    N'Mashhad,Tehran,Bandar,Shiraz and ...'  -- Description - nvarchar(max)
+    )
+	
+INSERT INTO dbo.MyTBSearch
+(Title,Description)
+VALUES
+(   N'Best city', -- Title - nvarchar(max)
+    N'Mashhad,Tehran,Bandar,Shiraz and ...'  -- Description - nvarchar(max)
+    )
+
+INSERT INTO dbo.MyTBSearch
+(Title,Description)
+VALUES
+(   N'Best city', -- Title - nvarchar(max)
+    N'Mashhad,Tehran,Bandar,Shiraz and ...'  -- Description - nvarchar(max)
+    )
+	GO
+    
+	
+SELECT * FROM dbo.MyTBSearch WHERE [Description] = N'Tehran' -- No result
+SELECT * FROM dbo.MyTBSearch WHERE [Description] LiKE N'%Tehran%' -- Worked
+SELECT * FROM dbo.MyTBSearch WHERE [Description] LiKE N'%Tehran Mashhad%' -- No result
+SELECT * FROM dbo.MyTBSearch WHERE FREETEXT ([Description], N'Tehran Mashhad') -- Worked  -- Used FullTextSearch
+
+SELECT SERVERPROPERTY('IsFullTextInstalled')  -- Check installed fullTextSearch service 

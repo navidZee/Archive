b0b8b63ca73c319a203500fc6eae8b29139f67cd
Navid Zare <55227087+navidZee@users.noreply.github.com>
2021-07-19 08:15:55 +0430

    codewars solutions

--- CSharpOOP_Tech/Program.cs
+++ CSharpOOP_Tech/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CSharpOOP_Tech
 {
@@ -6,13 +9,30 @@ namespace CSharpOOP_Tech
     {
         static void Main(string[] args)
         {
-            // For use vpaste.net link : // Use notepad and press CTRL + Shift (Ctrl + Right Shift sets the text direction to right-to-left. Ctrl + Left Shift sets the text direction to left-to-right.)
+            string[] a1 = new string[] { "arp", "live", "strong" };
+            string[] a2 = new string[] { "lively", "alive", "harp", "sharp", "armstrong" };
+
+            var r = inArray(a1, a2);
+
+
+
+
             Console.WriteLine("Hello World!");
+        }
 
+        public static string[] inArray(string[] array1, string[] array2)
+        {
+            List<string> r = new List<string>();
 
-
-
-
+            for (int index = 0; index < array1.Length; index++)
+            {
+                var item = array1[index];
+                if (array2.Any(arr => arr.Contains(item)))
+                {
+                    r.Add(item);
+                }
+            }
+            return r.Distinct().ToArray();
         }
     }
 }

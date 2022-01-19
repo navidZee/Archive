6f607734cae60a3060bf97f9276973f565031b8b
Navid Zare <55227087+navidZee@users.noreply.github.com>
2022-01-20 01:15:04 +0330

    add insertionSort

--- CSharpOOP_Tech/Program.cs
+++ CSharpOOP_Tech/Program.cs
@@ -10,7 +10,26 @@ namespace CSharpOOP_Tech
     {
         static void Main(string[] args)
         {
-            
+            var x = new int[] { 5, 3, 7, 0, -9, 16, 67, 13 };
+            InsertionSort(ref x);
+        }
+
+
+        private static void InsertionSort(ref int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i],
+                    j = i - 1;
+
+                while (j > -1 && array[j] > key)
+                {
+                    array[j + 1] = key; //swap
+                    j -= 1;
+                }
+
+                array[j + 1] = key;
+            }
         }
     }
 }

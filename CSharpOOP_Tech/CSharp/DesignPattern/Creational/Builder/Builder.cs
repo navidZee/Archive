def054664940d8d2c0475d8778d2342b887d81de
Navid Zare <55227087+navidZee@users.noreply.github.com>
2022-02-11 13:38:28 +0330

    Add builder design pattern

---
+++ CSharpOOP_Tech/CSharp/DesignPattern/Creational/Builder/Builder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace CSharpOOP_Tech.CSharp.DesignPattern.Creational
+{
+    public class Builder
+    {
+        void Main()
+        {
+            HttpHeaderBuilder builder = new HttpHeaderBuilder();
+            //QueryBuilder builder = new QueryBuilder();
+
+            ConstructionProcess(builder);
+        }
+        public void ConstructionProcess(IKeyValueGenerator builder)
+        {
+            builder.Add("name", "navid")
+                .Add("color", "red")
+                .Add("age", "21");
+        }
+    }
+
+    class HttpHeaderBuilder : IKeyValueGenerator
+    {
+        StringBuilder _queryStringBuilder = new StringBuilder();
+        public IKeyValueGenerator Add(string key, string value)
+        {
+            _queryStringBuilder.Append(key);
+            _queryStringBuilder.Append(":");
+            _queryStringBuilder.Append(value);
+            _queryStringBuilder.AppendLine();
+            return this;
+        }
+
+        public string Build => _queryStringBuilder.ToString();
+    }
+
+    class QueryBuilder : IKeyValueGenerator
+    {
+        StringBuilder _queryStringBuilder = new StringBuilder();
+        public IKeyValueGenerator Add(string key, string value)
+        {
+            _queryStringBuilder.Append(Convert.ToBoolean(_queryStringBuilder.Length) ? "?" : "=");
+            _queryStringBuilder.Append(key);
+            _queryStringBuilder.Append("=");
+            _queryStringBuilder.Append(Uri.EscapeDataString(value));
+            return this;
+        }
+
+        public string Build => _queryStringBuilder.ToString();
+    }
+}
---
+++ CSharpOOP_Tech/CSharp/DesignPattern/Creational/Builder/IKeyValueGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpOOP_Tech.CSharp.DesignPattern.Creational
+{
+    public interface IKeyValueGenerator
+    {
+        IKeyValueGenerator Add(string key, string value);
+    }
+}

eb4fc62384865f27b9ab3b0da8416fb77a363d6d
Navid Zare <55227087+navidZee@users.noreply.github.com>
2022-02-11 14:40:01 +0330

    add singleton

---
+++ CSharpOOP_Tech/CSharp/DesignPattern/Creational/Singleton/MemoryCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpOOP_Tech.CSharp.DesignPattern.Creational.Singleton
+{
+    public class MemoryCache
+    {
+        private static int i = 0;
+        private static MemoryCache _instance;
+        private static object _cacheLock = new object();
+
+        private MemoryCache()
+        {
+            Console.WriteLine("Created" + i++);
+        }
+
+        public static MemoryCache Create()
+        {
+            if (_instance is null)
+            {
+                lock (_cacheLock)
+                {
+                    if (_instance is null)
+                    {
+                        _instance = new MemoryCache();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
---
+++ CSharpOOP_Tech/CSharp/DesignPattern/Creational/Singleton/SingletonPattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOP_Tech.CSharp.DesignPattern.Creational.Singleton
+{
+    public class SingletonPattern
+    {
+        void Main()
+        {
+            int size = 8;
+            Task[] tasks = new Task[size];
+            for (int i = 0; i < size; i++)
+            {
+                tasks[i] = Task.Run(() => MemoryCache.Create());
+            }
+            Task.WaitAll(tasks);
+        }
+    }
+}

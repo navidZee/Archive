4b8347d9eb89f5bc1229155445d55ab0044f7e2c
Navid Zare <55227087+navidZee@users.noreply.github.com>
2022-05-08 11:46:34 +0430

    [ADD] add linear search

--- CSharpOOP_Tech/Program.cs
+++ CSharpOOP_Tech/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.SymbolStore;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 
 namespace CSharpOOP_Tech
 {
@@ -10,26 +6,6 @@ namespace CSharpOOP_Tech
     {
         static void Main(string[] args)
         {
-            var x = new int[] { 5, 3, 7, 0, -9, 16, 67, 13 };
-            InsertionSort(ref x);
-        }
-
-
-        private static void InsertionSort(ref int[] array)
-        {
-            for (int i = 1; i < array.Length; i++)
-            {
-                int key = array[i],
-                    j = i - 1;
-
-                while (j > -1 && array[j] > key)
-                {
-                    array[j + 1] = key; //swap
-                    j -= 1;
-                }
-
-                array[j + 1] = key;
-            }
         }
     }
 }

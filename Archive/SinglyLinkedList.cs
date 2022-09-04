728b728750efccce667c3006811319800981771d
Navid Zare <55227087+navidZee@users.noreply.github.com>
2022-09-04 19:55:28 +0430

    [ADD] add singlyLinkedList

---
+++ Archive/SinglyLinkedList.cs
@@ -0,0 +1,43 @@
+﻿namespace Archive
+{
+    partial class SinglyLinkedList
+    {
+        public class Node
+        {
+            public Node()
+            {
+                Next = null;
+            }
+
+            public object Data { get; set; }
+            public Node Next { get; set; }
+        }
+
+        private Node First { get => null; }
+
+        public void Push(object data)
+        {
+            var newNode = new Node()
+            {
+                Data = data,
+                Next = First
+            };
+
+        }
+
+        public Node List
+        {
+            get
+            {
+                Node current = First;
+                if (current == null)
+                    return null;
+
+                while (current.Next != null)
+                    current = current.Next;
+
+                return current;
+            }
+        }
+    }
+}
